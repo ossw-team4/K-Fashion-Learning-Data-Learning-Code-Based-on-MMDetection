4f0b074d9d896b3b7c60e05f3d77196a351bd12b
Inhwa Kim <radiantbeing99@gmail.com>
2022-12-16 13:13:11 +0900

    Train Dataset과 Validation Dataset 구분하도록 구성 변경

---
+++ configs/clothes/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_clothes.py
@@ -0,0 +1,30 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=12),
+        mask_head=dict(num_classes=12)))
+
+# Modify dataset related settings
+dataset_type = 'COCODataset'
+classes = ("coat", "jacket", "jumper", "cardigan", "blouse", 
+            "t-shirt", "sweater", "shirt", "onepiece(dress)",
+            "jumpsuite", "pants", "skirt",)
+data = dict(
+    train=dict(
+        img_prefix='data/clothes/images/',
+        classes=classes,
+        ann_file='data/clothes/train.json'),
+    val=dict(
+        img_prefix='data/clothes/images/',
+        classes=classes,
+        ann_file='data/clothes/val.json'),
+    test=dict(
+        img_prefix='data/clothes/images/',
+        classes=classes,
+        ann_file='data/clothes/val.json'))
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
